0e25589aeef54f373dc02ead28a4d73fdc73305d
ananyashekar98 <ananyashekar98@gmail.com>
2026-04-09 21:42:34 +0530

    Delete calculator.js

--- calculator.js
+++
@@ -1,9 +0,0 @@
-function divide(a, b) {
-  return a / b;  // Bug: no division by zero check!
-}
-
-function getUser(user) {
-  return user.name;  // Bug: no null check!
-}
-
-console.log(divide(10, 0));
